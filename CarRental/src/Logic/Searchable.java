fd98412c32c572d88b33b0e974ecf57e1cf9b3b7
Chris <walter.chris@gmail.com>
2016-03-10 13:26:07 -0600

    added interface

---
+++ CarRental/src/Logic/Searchable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author chriswalter
+ */
+public interface Searchable {
+    public boolean contains(String text);
+}
